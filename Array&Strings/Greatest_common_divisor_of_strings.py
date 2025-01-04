f5c6446675faa5b1b4743c3528c5d768872b67eb
Jason Chen <JasonYay210@gmail.com>
2025-01-04 15:27:00 -0500

    Solution to 1071

--- Array&Strings/Greatest_common_divisor_of_strings.py
+++ Array&Strings/Greatest_common_divisor_of_strings.py
@@ -1,10 +1,11 @@
 class Solution:
-    def gcdOfStrings(self, str1: str, str2: str) -> str:
-        # Question: 1071
+    # Question: 1071
 
-        # 5ms Beats 13.26%
-        # Time Complexity: O(L * (n + m)), where L = min(len(str1), len(str2)), n = len(str1), and m = len(str2)
-        # Space Complexity: O(L), where L = min(len(str1), len(str2)) 
+    # 5ms Beats 13.26%
+    # Time Complexity: O(L * (n + m)), where L = min(len(str1), len(str2)), n = len(str1), and m = len(str2)
+    # Space Complexity: O(L), where L = min(len(str1), len(str2)) 
+    
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
 
         # Check if concatenated strings are equal or not; if not, return ""
         if str1 + str2 != str2 + str1:
