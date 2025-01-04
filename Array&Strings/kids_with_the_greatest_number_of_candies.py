f717cc7957a0452e528d0a29ed5d23589e3cd36d
Jason Chen <JasonYay210@gmail.com>
2025-01-04 15:18:44 -0500

    Solution to 1431

--- Array&Strings/kids_with_the_greatest_number_of_candies.py
+++ Array&Strings/kids_with_the_greatest_number_of_candies.py
@@ -1,6 +1,10 @@
 class Solution:
 
-    #
+    # Question: 1431
+
+    # 0ms Beats 100%
+    # Time Complexity: O(n)
+    # Space Complexity: O(n)
 
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
         M = max(candies)
