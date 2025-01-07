679248a39098ef2df5e0aeb34065a2a57bee69cb
Jason Chen <JasonYay210@gmail.com>
2025-01-07 16:30:41 -0500

    Solution to 392

--- Array&Strings/is_subsequence.py
+++ Array&Strings/is_subsequence.py
@@ -4,11 +4,13 @@ class Solution:
     # 0ms Beats 100%
     # Time Complexity: O(n)
     # Space Complexity: O(1)
-    
+
     def isSubsequence(self, s: str, t: str) -> bool:
         count = 0
+        
         if s == '':
             return True
+        
         for i in t:
             if s[count] == i:
                 count +=1
