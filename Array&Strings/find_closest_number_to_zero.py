1feb9083bec08c7cc5f42334ea0cb6065a8d5367
Jason Chen <JasonYay210@gmail.com>
2025-01-08 22:44:16 -0500

    temp

---
+++ Array&Strings/find_closest_number_to_zero.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findClosestNumber(self, nums: List[int]) -> int:
+        # Question: 605
+
+        # 14ms Beats 17.80%
+        # Time Complexity: O(n)
+        # Space Complexity: O(1)
+
+
+        n = len(nums) 
+        result = float("inf")
+        if n == 1:
+            return nums[0]
+
+        # [2,-1,1] 
+        for i in range(n):
+            if abs(nums[i]) < result:
+                result = abs(nums[i])
+        
+        if result not in nums:
+            return result *-1
+            
+        return result
+            
