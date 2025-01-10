b99933d62390fda95286da0870d6c1f10ca852ed
Jason Chen <JasonYay210@gmail.com>
2025-01-09 21:32:10 -0500

    temp

---
+++ Array&Strings/roman_to_integer.py
@@ -0,0 +1,26 @@
+class Solution:
+    # Question: 151
+
+    # 7ms Beats 43.30%
+    # Time Complexity: O(n)
+    # Space Complexity: O(n)
+    def romanToInt(self, s: str) -> int:
+        my_dict = {
+            'I':1,
+            'V':5,
+            "X":10,
+            "L":50,
+            "C":100,
+            "D":500,
+            "M":1000
+        }
+        result = 0
+        i = 0
+        while i < len(s):
+            if i != len(s)-1 and my_dict[s[i+1]] > my_dict[s[i]]:
+                result += (my_dict[s[i+1]] - my_dict[s[i]])
+                i += 2
+            else:
+                result += my_dict[s[i]]
+                i +=1
+        return result 
