2c65f567bea4e6f818b0ca29f0d9efe1a8aff15d
Jason <JasonYay210@gmail.com>
2025-01-13 19:42:32 -0500

    Solution to 14

--- Array&Strings/longest_common_prefix.py
+++ Array&Strings/longest_common_prefix.py
@@ -7,12 +7,12 @@ class Solution:
     # 0ms Beats 100%
     # Time Complexity: O(n)
     # Space Complexity: O(1)
-
+    
     def longestCommonPrefix(self, strs: List[str]) -> str:
         min_length = float('inf')
         for s in strs:
             if min_length > len(s):
-                min_length = len(s
+                min_length = len(s)
         i = 0
         while i < min_length:
             for s in strs:
