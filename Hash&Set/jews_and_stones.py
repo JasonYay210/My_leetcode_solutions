c6fd4da2532efc1f68cde0caa547ac16ef3d7d12
Jason <JasonYay210@gmail.com>
2025-01-19 16:49:57 -0500

    Solution to 771

---
+++ Hash&Set/jews_and_stones.py
@@ -0,0 +1,11 @@
+class Solution:
+    # Question: 121
+
+    # 24ms Beats 92.18%
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
+
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        set_jewels = set(jewels)
+
+        return sum(s in set_jewels for s in stones)
