f3e51596f5c62da449df25b2e4cc1ad7fd2ed128
Jason <JasonYay210@gmail.com>
2025-01-20 13:50:58 -0500

    Solution to 217

--- Hash&Set/contains_duplicate.py
+++ Hash&Set/contains_duplicate.py
@@ -2,6 +2,11 @@ from typing import List
 
 
 class Solution:
+    # Question: 771
+
+    # 0ms Beats 100%
+    # Time Complexity: O(n + m) m being set_jewels
+    # Space Complexity: O(m)
     def containsDuplicate(self, nums: List[int]) -> bool:
         set_nums = set()
         for num in nums:
