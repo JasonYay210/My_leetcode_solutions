807b436aaf57f170a4721e43240dbc824753039e
Jason <JasonYay210@gmail.com>
2025-01-21 08:51:30 -0500

    Solution to 383

---
+++ Hash&Set/ransom_note.py
@@ -0,0 +1,17 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        C_ranson = defaultdict(int)
+
+        for item in ransomNote:
+            C_ranson[item] += 1
+
+        c_magazine = defaultdict(int)
+
+        for item in magazine:
+            c_magazine[item] +=1
+
+        for i in C_ranson.keys():
+            if C_ranson[i] > c_magazine[i]:
+                return False
+        return True
+            
