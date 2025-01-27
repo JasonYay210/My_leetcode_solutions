1831be231c88132987223c8fe5f26f87b896a289
Jason <JasonYay210@gmail.com>
2025-01-27 10:24:32 -0500

    Solution to 169

--- Hash&Set/majority_element.py
+++ Hash&Set/majority_element.py
@@ -3,9 +3,8 @@ from typing import Counter, List
 
 class Solution:
 
-    
+
     def majorityElement(self, nums: List[int]) -> int:
-        result = 0
         counter = Counter(nums)
         for i in counter:
             if counter[i] > len(nums) / 2:
